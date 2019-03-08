934133fd7fa5657e7f99d3a4cd6258466c2e66b3
Frenzoid <frenzoid@protonmail.com>
2019-03-08 16:34:53 +0100

    Update ejercicio6.m

--- Practica 1-2/ejercicio6.m
+++ Practica 1-2/ejercicio6.m
@@ -2,6 +2,6 @@
 
 syms x;
 func6 = @(a,b,c) a + b * x + c * x ^ 2;
-ezplot(func6(2,4,3)); % Usamos ezplot en este caso porque solo hay una icognita, y por ello no es necesario verlo en 3D.
+ezplot(func6(2,4,3)); % Usamos ezplot en este caso porque solo hay una icognita (x), y por ello no es necesario verlo en 3D.
 
 clear;
