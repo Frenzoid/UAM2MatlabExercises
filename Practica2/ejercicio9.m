3a5cf7198dc1e35e86c60cc4dadd87f51684a7f0
Frenzoid <frenzoid@protonmail.com>
2019-03-09 18:21:20 +0100

    Create ejercicio9.m

---
+++ Practica2/ejercicio9.m
@@ -0,0 +1,26 @@
+% Ejercico 9.
+
+syms m b x;
+f(x) = x^2 + 1
+ezplot(f);
+hold on;
+plot(0, 2, 'ro');
+
+numeros = -10:10;
+resultados = subs(f, x, numeros);
+
+minimoIterador = 0;
+minVal = inf;
+
+for i = 1:size(numeros)
+    if numeros(i) + resultados(i) < minVal
+        minimoIterador = i;
+        valor = numeros(i) + resultados(i);
+        minVal = valor;
+    end
+
+end
+
+plot(numeros(minimoIterador), resultados(minimoIterador), 'ro');
+
+% Falta arreglarlo.
