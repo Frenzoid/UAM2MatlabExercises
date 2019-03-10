450cc2a99a40d3d9bebe8bafb82e65b860baf476
Frenzoid <frenzoid@protonmail.com>
2019-03-10 15:12:13 +0100

    Update ejercicio4.m

--- ejerciciosRepasoT1-2/ejercicio4.m
+++ ejerciciosRepasoT1-2/ejercicio4.m
@@ -1,9 +1,10 @@
+% Ejercicio 4.
+
 vector = input('Dame el vector a procesar: ');
 rangoLimite = input('Dame el rango limite: ');
 
 % Si es mayor que el rango, lo recortamos (truncar)
 if length(vector) > rangoLimite
-    disp(vector(1:rangoLimite))
     vector = vector(1:rangoLimite);
 end
 
