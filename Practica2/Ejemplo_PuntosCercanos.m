72d39c8a36b031ed4be56c86a1525e90b193228e
Frenzoid <frenzoid@protonmail.com>
2019-03-12 20:12:28 +0100

    Update Ejemplo_PuntosCercanos.m

--- Practica2/Ejemplo_PuntosCercanos.m
+++ Practica2/Ejemplo_PuntosCercanos.m
@@ -7,9 +7,15 @@ yi(1) = 3.5; yi(2) = 4; yi(3) = 8; yi(4) = 9.5; yi(5) = 10;
 yi(6) = 12; yi(7) = 14; yi(8) = 16; yi(9) = 18.5; yi(10) = 20;
 
 % Mostramos los puntos.
-plot(xi, yi,’ro’);
+plot(xi, yi,'ro');
 hold on;
 
 % Calculamos los puntos cercanos.
-yi == m*xi + b
+f(m) = sum((yi - m * xi).^2);
 
+f1 = diff (f , m);
+
+respuesta = double(solve(f1 == 0));
+
+% Imprimimos la linea mas cercana a los puntos.
+ezplot(@(x) respuesta * x, [0, 13]);
