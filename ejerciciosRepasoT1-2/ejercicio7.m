32b712a47c5d0ab4f32268c88805f7985061e7a5
Frenzoid <frenzoid@protonmail.com>
2019-03-12 20:08:01 +0100

    Update ejercicio7.m

--- ejerciciosRepasoT1-2/ejercicio7.m
+++ ejerciciosRepasoT1-2/ejercicio7.m
@@ -1,31 +1,3 @@
-% Ejercicio 7.
-
-% Nos piden el precio minimo de la dimension del solar para poder sacar el precio minimo.
-
-% Area casa: 100 = x * y)
-% AreaSolar: (8 + y) * (10 + x);
-% Coste Solar: AreaSolar * 1000;
-
-% Despejamos y:
-    % 100 / x = y;
-    
-% Substituimos:
-    % (8 + 100 / x) * (10 + x) * 1000;
-    
-% Calculamos;
-    % 80 + 8x + 1000/x + 100 * 1000
-    
-% Derivamos:
-    % 8 - 1000/x^2
-    
-% Igualamos a 0 y sacamos los criticos:
-    % 8 * x^2 = 1000;
-    % x^2 = 1000/8;
-    % x = +raiz(1000/8);
-    
-    
-    
-    
 syms x y;
 
 f = (8 + y) * (10 + x) * 1000;
@@ -38,6 +10,6 @@ fcrits = double(solve(f1 == 0));
 
 fcrits = fcrits(2); % El valor positivo.
 
-fprintf('Area del solar: %.2f m2, precio del solar %.2f eur. \n',fcrits, subs(f,x,fcrits));
+fprintf('Area del solar: %.2f m2, precio del solar %.2f eur. \n',subs(f,x,fcrits) / 1000, subs(f,x,fcrits));
  
     
