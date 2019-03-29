fd90b922acf4ea2221fdcda41a070fd90c61c763
Frenzoid <frenzoid@protonmail.com>
2019-03-29 16:49:17 +0100

    Update ejercicio 4.m

--- Practica 3/examen/ejercicio 4.m
+++ Practica 3/examen/ejercicio 4.m
@@ -1,19 +1,20 @@
- % Como todos ya sabemos que L = sin(sqrt(1 + diff(x)^2), a, b) dx, calcula la longitud de la curva y = nthroot(x^2,3) para
+% Ejercicio 4
+
+% Como todos ya sabemos que L = sin(sqrt(1 + diff(x)^2), a, b) dx, calcula la longitud de la curva y = nthroot(x^2,3) para
 %   el intervalo 0 < x < 4;
 
-%Ejercicio 4
 
-%Longitud de la curva de Y
+% Longitud de la curva de Y
 
 syms x;
-y = @(x) nthroot(x^2,3); %Convertimos en función anónima
-dy = diff(y);            %Hacemos la primera derivada
-F = sqrt(1 + dy^2);      %Lo ponemos todo dentro de una variable igual que en la fórmula
+y = @(x) nthroot(x^2,3); % Convertimos en función anónima
+dy = diff(y);            % Hacemos la primera derivada
+F = sqrt(1 + dy^2);      % Lo ponemos todo dentro de una variable igual que en la fórmula
 
-%Mínimos y Máximos de X
+% Mínimos y Máximos de X
 
 a = 0;
 b = 2;
 
 
-L = int(F,x,a,b)        %Igualamos L a la integral definida de la variable F entre a y b 
+L = int(F,x,a,b)        % Igualamos L a la integral definida de la variable F entre a y b 
