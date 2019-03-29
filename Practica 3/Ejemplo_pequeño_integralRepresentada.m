7084492241913416e6b13893e7999f12dc7ec681
Frenzoid <frenzoid@protonmail.com>
2019-03-30 00:11:49 +0100

    Update Ejemplo_pequeño_integralRepresentada.m

--- Practica 3/Ejemplo_pequeño_integralRepresentada.m
+++ Practica 3/Ejemplo_pequeño_integralRepresentada.m
@@ -1,36 +1,49 @@
-f = @(x) x.^2 + 1;
+% La funcion a calcular.
+f = @(x) x.^2 + 1; 
 
-a = 1; % Punto inicio de la grafica.
-b = 9; % Punto final de la grafica.
-n = 10; % Cantidad de divisiones (cuantos rectangulos)
+a = 1; % Punto de inicio.
+b = 9; % Punto Final.
+n = 10; % Numero de rectangulos. (Mientras mas numeros, mas cerca del valor exacto).
 
-xi = linspace(a, b, n + 1);
-h = (b - a)/n;
+% Guardamos en una matriz, los valores de n+1 de 'a' a 'b'. (Haz un disp de xi para entenderlo).
+xi = linspace(a, b, n+1); 
 
-ezplot(f(x), [a - 1, b + 1]);
-axis(double([a - 1, b + 1, 0 f(b + 1)]));
+% Sacamos la altura media.
+h = (b - a)/n; 
 
+% Mostramos la grafica.
+ezplot(f, [a, b]); 
+
+% Anclamos el grafico.
 hold on;
 
-for i = 1 : n
-    Ll(i) = plot([xi(i) xi(i)], double([0 f(xi(i))]), 'g');
-    Lt(i) = plot([xi(i) xi(i + 1)], double([f(xi(i)) f(xi(i))]), 'g');
-    Lr(i) = plot([xi(i + 1) xi(i + 1)], double([0 f(xi(i))]), 'g');
+% Hacemos el bucle que dibuja los rectangulos
+for i = 1 : n 
+    % Rectangulos de la izquierda.
+    plot([xi(i) xi(i)], double([0 f(xi(i))]), 'r');
+    plot([xi(i) xi(i+1)], double([f(xi(i)) f(xi(i))]), 'r');
+    plot([xi(i+1) xi(i+1)], double([0 f(xi(i))]), 'r');
     
-    Rl(i) = plot([xi(i) xi(i)], double([0 f(xi(i + 1))]), 'r');
-    Rt(i) = plot([xi(i) xi(i + 1)], double([f(xi(i + 1)) f(xi(i + 1))]), 'r');
-    Rr(i) = plot([xi(i + 1) xi(i + 1)], double([0 f(xi(i + 1))]), 'r');
+    % Rectangulos de la derecha.
+    plot([xi(i) xi(i)], double([0 f(xi(i+1))]), 'g');
+    plot([xi(i) xi(i+1)], double([f(xi(i+1)) f(xi(i+1))]), 'g');
+    plot([xi(i+1) xi(i+1)], double([0 f(xi(i+1))]), 'g');
 end
 
-for i = 1 : n + 1
+% Calculamos el sumatorio total.
+for i = 1:n + 1
     yi(i) = f(xi(i));
 end
 
-Ln = h * sum(double(yi(1 : n)));
-Rn = h * sum(double(yi(2 : n + 1)));
-I = double(int(f, a, b));
+% Hacemos los calculos de la izq, y de la derecha.
+Ln = h * sum(double(yi(1:n))); % Nos dará el valor aproximado por la izquierda.
+Rn = h * sum(double(yi(1:n + 1))); % Nos dará el valor aproximado por la derecha.
 
-fprintf('Ln = %.2f, Rn = %.2f, I = %.2f \n', Ln, Rn, I);
+% Hacemos el valor exacto con una integral. (nos dará el valor exacto).
+I = double(int(f, x, a, b));
 
-title([n ," rectangulo/s"]);
+% Mostramos los resultados.
+fprintf('Ln = %.2f, Rn = %.2f, Valor al que se aproxima = %.2f \n', Ln, Rn, I);
+
+% Desanclamos el grafico.
 hold off;
