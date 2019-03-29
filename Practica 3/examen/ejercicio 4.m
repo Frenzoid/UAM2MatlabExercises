b9f6cc692dabf26387e45d2e0f9ac4a1c19570b3
Frenzoid <frenzoid@protonmail.com>
2019-03-29 16:48:49 +0100

    Update ejercicio 4.m

--- Practica 3/examen/ejercicio 4.m
+++ Practica 3/examen/ejercicio 4.m
@@ -1,4 +1,4 @@
-% Como todos ya sabemos que L = sin(sqrt(1 + diff(x)^2), a, b) dx, calcula la longitud de la curva y = sqrt(x^2)^3 para
+ % Como todos ya sabemos que L = sin(sqrt(1 + diff(x)^2), a, b) dx, calcula la longitud de la curva y = nthroot(x^2,3) para
 %   el intervalo 0 < x < 4;
 
 %Ejercicio 4
@@ -6,7 +6,7 @@
 %Longitud de la curva de Y
 
 syms x;
-y = @(x)nthroot(x^2,3);  %Convertimos en función anónima
+y = @(x) nthroot(x^2,3); %Convertimos en función anónima
 dy = diff(y);            %Hacemos la primera derivada
 F = sqrt(1 + dy^2);      %Lo ponemos todo dentro de una variable igual que en la fórmula
 
