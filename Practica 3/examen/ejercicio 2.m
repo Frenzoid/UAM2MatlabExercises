797fe91d498498985c04cecee2f315b658f3ed34
Frenzoid <frenzoid@protonmail.com>
2019-03-29 16:28:56 +0100

    Update ejercicio 2.m

--- Practica 3/examen/ejercicio 2.m
+++ Practica 3/examen/ejercicio 2.m
@@ -1,18 +1,13 @@
-% Calcula si la inegral indefinida int(1/(x^2+5*x+4), x, 4, inf) converge o diverge. En caso de converger, cual es su valor?
-
-%Ejercicio 2
-
-%Converge o diverge
+% Ejercicio 2.
+% Calcula si la inegral indefinida int(1/(x^2+5*x+4), x, 4, inf) converge o diverge. 
+% En caso de converger, cual es su valor?
 
 syms x;
-den = x^2 + 5*x + 4;
-f = @(x)1/den;
 
-value = double(int(f,x,4,inf))
+result = int(1/(x^2+5*x+4), x, 4, inf);
 
-if isreal(value)
-        fprintf('Converge ya que es real, y el valor es: %.2f \n', value);
-    A = 1;    
+if isreal(result)
+    disp(double(result))
 else
-    fprintf('Diverge ya que no es real \n');
+    disp('diverge')
 end
