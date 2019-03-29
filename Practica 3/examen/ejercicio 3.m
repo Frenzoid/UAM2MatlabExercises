fdf8ce2e07c019fef46b477d498bb7bf4b55e6c1
Frenzoid <frenzoid@protonmail.com>
2019-03-29 16:33:52 +0100

    Update ejercicio 3.m

--- Practica 3/examen/ejercicio 3.m
+++ Practica 3/examen/ejercicio 3.m
@@ -1,26 +1,12 @@
+% Ejercicio 3.
 
 % Utilizando unicamente el comando "int" Calcula el valor decimal de: int(int(int(x^2 + sin(y) * sin(z)))) dx dy xz. 
 %   con D = {(x,y,z) = 0 < x < sqrt(5); 0 < y < 2 * pi; 0 < z < arctan(2);
 
-% Ejercicio 3.
-
-%Integral triple con valor décimal en el dominio D siendo X entre 0 y raiz
-%de 5, Y entre 0 y 2pi y Z entre 0 y arcotangente de 2
 syms x y z;
-fun = @(x,y,z)x^2 + sin(y)*sin(z); %Convertimos en función anónima
 
-%Mínimos y Máximos de X
-a = 0;
-b = sqrt(5);
+f = x^2 + sin(y) * sin(z);
 
-%Mínimos y Máximos de Y
-c = 0;
-d = 2*pi;
+result = double(int(int(int(f, x, 0, sqrt(5)), y, 0, 2), z, 0, atan(2)));
 
-%Mínimos y Máximos de Z
-
-e = 0;
-f = atan(2);
-
-value = double(int(int(int(fun,z,e,f),y,c,d),x,a,b));
-disp(value)
+disp(result);
