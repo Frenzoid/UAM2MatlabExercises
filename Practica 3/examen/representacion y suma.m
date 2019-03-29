1a48c06effb77e7b6dad66dbc95def7773d7592c
Frenzoid <frenzoid@protonmail.com>
2019-03-29 17:13:59 +0100

    Create representacion y suma.m

---
+++ Practica 3/examen/representacion y suma.m
@@ -0,0 +1,36 @@
+
+syms x;
+f = @(x) x.^2 + 1;
+fexpression = x^2 + 1;
+
+a = 1;
+b = 9;
+n = 4;
+
+xi = linspace(a, b, n+1);
+
+h = (b - a)/2;
+
+ezplot(f, [a, b]);
+hold on;
+
+for i = 1:n
+    plot([xi(i) xi(i)], double([0 f(xi(i))]), 'r');
+    plot([xi(i) xi(i+1)], double([f(xi(i)) f(xi(i))]), 'r');
+    plot([xi(i+1) xi(i+1)], double([0 f(xi(i))]), 'r');
+    
+    plot([xi(i) xi(i)], double([0 f(xi(i+1))]), 'g');
+    plot([xi(i) xi(i+1)], double([f(xi(i+1)) f(xi(i+1))]), 'g');
+    plot([xi(i+1) xi(i+1)], double([0 f(xi(i+1))]), 'g');
+end
+
+for i = 1:n+1
+    yi(i) = f(xi(i));
+end
+
+Ln = h * sum(double(yi(1:n)));
+Rn = h * sum(double(yi(1:n + 1)));
+
+I = double(int(fexpression, a, b));
+
+fprintf('Ln = %.2f, Rn = %.2f, I = %.2f \n', Ln, Rn, I);
