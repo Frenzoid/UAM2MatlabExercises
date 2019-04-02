798400487cf7136186ad51c15e1d342d9382ce23
Frenzoid <frenzoid@protonmail.com>
2019-04-02 18:46:44 +0200

    Update ejercicio 4.m

--- Practica 3/examen/ejercicio 4.m
+++ Practica 3/examen/ejercicio 4.m
@@ -7,8 +7,8 @@
 % Longitud de la curva de Y
 
 syms x;
-y = @(x) nthroot(x^2,3); % Convertimos en función anónima
-dy = diff(y);            % Hacemos la primera derivada
+y = @(x) (x^2)^(1/3);    % Convertimos en función anónima
+dy = diff((x^2)^(1/3));  % Hacemos la primera derivada
 F = sqrt(1 + dy^2);      % Lo ponemos todo dentro de una variable igual que en la fórmula
 
 % Mínimos y Máximos de X
@@ -17,4 +17,4 @@ a = 0;
 b = 2;
 
 
-L = int(F,x,a,b)        % Igualamos L a la integral definida de la variable F entre a y b 
+L = double(int(F,x,a,b)) % Igualamos L a la integral definida de la variable F entre a y b 
