36bcd2bc3044a940758b6b40bade80809d896be7
Frenzoid <frenzoid@protonmail.com>
2019-04-05 10:32:20 +0200

    Create RegulaFalsi1.m

---
+++ Practica 4/RegulaFalsi1.m
@@ -0,0 +1,40 @@
+function outputValue = Secant1(f, a, b, tolerancia, errorfun, maxiter)
+    % Ejemplo.
+    
+    if f(a) * f(b) < 0 % Comprueba si la funcion converge dentro del intervalo.
+
+        i = 1;
+        h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
+        c = (a + b) / 2;
+       
+        while ~(abs(f(c)) <= errorfun) && ~(h <= tolerancia) && i < maxiter && i < 4
+
+            if abs(f(a)) > abs(f(b)) % Si f(a) es mayor que f(b) intercambiamos a por b, y b por a.
+                bridgeVar = a; % Variable que usaremos de puente para intercambiar a y b dentro del bucle.
+                a = b;
+                b = bridgeVar;
+            end
+            
+            h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
+            c = a - h;
+            b = c;
+            
+            if f(a) * f(b) < 0
+                b = c; 
+            else
+                a = c;
+            end
+
+            i = i + 1;
+        end
+        
+        % Descomenta esto para ver los datos en detalle referente al resultado (ULTIMA ITERACION).
+        % fprintf('| i |   a  |   c  |   b  |   (b-a)/2  | \n', i, a, c, b, rango); 
+        % disp('------------------------------------------');
+        % fprintf('| %1.0f | %.2f | %.2f | %.2f |    %.2f    | \n', i, a, c, b, h); 
+        outputValue = c;
+        
+    else
+        disp('Intervalo no valido'); % No hay convergencia en el intervalo introducido.
+    end
+end
