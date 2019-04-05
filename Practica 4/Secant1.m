bc75c810d3f8222b05c99f62aceb80f4a95f667c
Frenzoid <frenzoid@protonmail.com>
2019-04-05 16:44:51 +0200

    Update Secant1.m

--- Practica 4/Secant1.m
+++ Practica 4/Secant1.m
@@ -5,7 +5,8 @@ function outputValue = Secant1(f, a, b, tolerancia, errorfun, maxiter)
         h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
         c = (a + b) / 2;
        
-        while ~(abs(f(c)) <= errorfun) && ~(h <= tolerancia) && i < maxiter && i < 4
+        % arreglar abs(h) <= tolerancia.
+        while ~( abs( f(c) ) <= errorfun ) && ~( abs(h) <= tolerancia ) && i < maxiter && i < 4
 
             if abs(f(a)) > abs(f(b)) % Si f(a) es mayor que f(b) intercambiamos a por b, y b por a.
                 bridgeVar = a; % Variable que usaremos de puente para intercambiar a y b dentro del bucle.
@@ -17,12 +18,12 @@ function outputValue = Secant1(f, a, b, tolerancia, errorfun, maxiter)
             c = a - h;
             b = c;
 
+            
+            % Descomenta esto para ver los datos en detalle referente al resultado (ULTIMA ITERACION).
+            fprintf('| %1.0f | %.2f | %.2f | %.2f |    %.2f    | \n', i, a, c, b, h); 
+            
             i = i + 1;
         end
         
-        % Descomenta esto para ver los datos en detalle referente al resultado (ULTIMA ITERACION).
-        % fprintf('| i |   a  |   c  |   b  |   (b-a)/2  | \n', i, a, c, b, rango); 
-        % disp('------------------------------------------');
-        % fprintf('| %1.0f | %.2f | %.2f | %.2f |    %.2f    | \n', i, a, c, b, h); 
         outputValue = c;
 end
