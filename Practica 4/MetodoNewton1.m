db3093c9c7797b292bb6f83442fda6b9a451669f
Frenzoid <frenzoid@protonmail.com>
2019-04-05 10:36:19 +0200

    Create MetodoNewton1.m

---
+++ Practica 4/MetodoNewton1.m
@@ -0,0 +1,22 @@
+function outputValue = Secant1(f, a, b, tolerancia, errorfun, maxiter)
+    % Ejemplo.
+           
+        i = 1;
+        h = f(a) / diff(f(a));
+        c = a - h;
+       
+        while ~(abs(f(c)) <= errorfun) && ~(h <= tolerancia) && i < maxiter && i < 4
+
+            i:=i+1
+            h = f(a) / diff(f(a));
+            c = a - h;
+            a = c;    
+        
+        end
+        
+        % Descomenta esto para ver los datos en detalle referente al resultado (ULTIMA ITERACION).
+        % fprintf('| i |   a  |   c  |   b  |   (b-a)/2  | \n', i, a, c, b, rango); 
+        % disp('------------------------------------------');
+        % fprintf('| %1.0f | %.2f | %.2f | %.2f |    %.2f    | \n', i, a, c, b, h); 
+        outputValue = c;
+end
