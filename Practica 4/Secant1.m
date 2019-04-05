931fa440a18cc4e6887f96f9e2f3c5d33d8d61f3
Frenzoid <frenzoid@protonmail.com>
2019-04-05 10:13:01 +0200

    Update Secant1.m

--- Practica 4/Secant1.m
+++ Practica 4/Secant1.m
@@ -7,7 +7,7 @@ function outputValue = Secant1(f, a, b, tolerancia, errorfun, maxiter)
        
         while ~(abs(f(c)) <= errorfun) && ~(h <= tolerancia) && i < maxiter && i < 4
 
-            if abs(f(a)) > abs(f(b)) % Si f(a) es mayor que f(B) intercambiamos a por b, y b por a.
+            if abs(f(a)) > abs(f(b)) % Si f(a) es mayor que f(b) intercambiamos a por b, y b por a.
                 bridgeVar = a; % Variable que usaremos de puente para intercambiar a y b dentro del bucle.
                 a = b;
                 b = bridgeVar;
