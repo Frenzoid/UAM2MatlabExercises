21825a97e5609283226ec4e695b6d542743f3ada
Frenzoid <frenzoid@protonmail.com>
2019-04-05 03:14:06 +0200

    Update ejercicio 2.m

--- Practica 4/ejercicio 2.m
+++ Practica 4/ejercicio 2.m
@@ -1,31 +1,46 @@
 function outputValue = Biseccion(f, a, b, tolerancia, errorfun, maxiter)
-
-    i = 0;
-    DATA.length = 0;
-    
-    while ~(abs((a-b)/2) <= tolerancia) && ~(abs((b - a)/2) <= errorfun) && ~(i == 4)
+   
+    if f(a) * f(b) < 0
         
+        DATA.length = 1; % Inicializamos el contador de "objetos" (y a su vez definimos el objeto padre DATA).
+        i = 1; 
+        rango = abs(b - a);
         c = (a + b) / 2;
+       
         
-        if f(a) * f(b) < 0 
-            b = c;
-            a = a;
-        else
-            a = c;
-            b = b;
+        while ~(abs(f(c)) <= errorfun) && ~(rango <= tolerancia) && i < maxiter && i < 4
+
+            c = (a + b) / 2; 
+            rango = abs(b - a) / 2;
+
+            
+            if f(a) * f(b) < 0 
+                b = c;         
+            else
+                a = c;
+            end
+            
+            DATA.a(i) = a; % valor de inicio del intervalo en esta iteracion.
+            DATA.b(i) = b; % valor de final del intervalo en esta iteracion.
+            DATA.c(i) = c; % valor de c.
+            DATA.i(i) = i; % valor del contador.
+            DATA.h(i) = rango;  % valor del rango.
+            DATA.length = DATA.length + 1; % incrementamos la cantidad de datos.
+            
+            i = i + 1;
+        end
+               
+        disp('------------------------------------------');
+        fprintf('| i |   a  |   c  |   b  |   (b-a)/2  | \n', i, a, c, b, rango); 
+        disp('------------------------------------------');
+        
+        for i = 1:length(DATA.length)
+            fprintf('| %1.0f | %.2f | %.2f | %.2f |    %.2f    | \n', i, a, c, b, rango);         
         end
         
-        DATA.a(i) = a;
-        DATA.b(i) = b;
-        DATA.c(i) = c;
-        DATA.i(i) = i;
-        DATA.h(i) = (b - a)/2;
-        DATA.length = DATA.length + 1;
-
-        i = i + 1;
-    end
-    
-    for i = 1:length(DATA.length)
-        fprintf('i = %.2f, a = %.2f, c = %.2f, b = %.2f, (b-a)/2 = %.2f \n', DATA.i(i), DATA.a(i), DATA.c(i), DATA.b(i), DATA.h(i)); 
+        outputValue = c; 
+    else
+        disp('Intervalo no valido');
     end
 end
+
