15c49b56829c7b775d4f3e3538c09622a3c33a45
Frenzoid <frenzoid@protonmail.com>
2019-04-05 03:00:29 +0200

    Update ejercicio 1.m

--- Practica 4/ejercicio 1.m
+++ Practica 4/ejercicio 1.m
@@ -1,18 +1,46 @@
 function outputValue = Biseccion(f, a, b, tolerancia, errorfun, maxiter)
+    % outputValue: Variable de salida.
+    % f: funcion a procesar.
+    % a: inicio del intervalo.
+    % b: fin del intervalo.
+    % tolerancia: distancia minima entre a y b (b - a).
+    % errorfun: error minimo de la funcion (cantidad de decimales que queires que tu resultado tenga.
+   
+    if f(a) * f(b) < 0 % Comprueba si la funcion converge dentro del intervalo.
+        
+        i = 1; % Contador de iteradores.
+        rango = abs(b - a); % rango del intervalo. ('h' en los apuntes.)
+        c = (a + b) / 2; % Inicializamos 'c' para poder usarlo en la condicion (porque matlab no soporta do...while).
+        
+        % Condicion de salida.
+        % Mientras que f(x) no sea menor o igual a errorfun introducido, Y 
+        % Mientras que el rango no sea menor o igual a la tolerancia introducida, Y 
+        % Mientras que las iteraciones sean menores que 4, Y 
+        % Mientras que las iteraciones sean menores que maxiter,
+        % continuamos el bucle.
+        
+        while ~(abs(f(c)) <= errorfun) && ~(rango <= tolerancia) && i < maxiter && i < 4
 
-    i = 0;
-    while ~(abs((a-b)/2) <= tolerancia) && ~(abs((b - a)/2) <= errorfun) && ~(i == 4)
-        c = (a + b) / 2;
-        if f(a) * f(b) < 0 
-            b = c;
-            a = a;
-        else
-            a = c;
-            b = b;
+            c = (a + b) / 2; % Por cada iteracion, actualizamos el punto de enmedio.
+            rango = abs(b - a) / 2; % Por cada iteracion, actualizamos el rango.
+
+            
+            if f(a) * f(b) < 0 % Si el resultado es menor que 0, la convergencia esta en la primera mitad del intervalo. De a hasta la mitad de a y b).
+                b = c;         %  si no es 0, la convergencia esta en la segunda mitad del intervalo. De la mitad de a y b hasta b.
+            else
+                a = c;
+            end
+            
+            i = i + 1;
         end
-        fprintf('i = %.2f, a = %.2f, c = %.2f, b = %.2f, (b-a)/2 = %.2f \n', i, a, c, b, (b - a)/2); 
-        i = i + 1;
+        
+        % Descomenta esto para ver los datos en detalle referente al resultado (ULTIMA ITERACION).
+        % fprintf('| i |   a  |   c  |   b  |   (b-a)/2  | \n', i, a, c, b, rango); 
+        % disp('------------------------------------------');
+        % fprintf('| %1.0f | %.2f | %.2f | %.2f |    %.2f    | \n', i, a, c, b, rango); 
+        outputValue = c; % Devolvemos c (que seria el valor aproximado para x para que la funcion cumpla las condiciones.
+    else
+        disp('Intervalo no valido'); % No hay convergencia en el intervalo introducido.
     end
-
 end
 
