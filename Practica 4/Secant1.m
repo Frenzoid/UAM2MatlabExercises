5ac8257bb2a9e7a0fcad27f06761e2eeaf574665
Frenzoid <frenzoid@protonmail.com>
2019-04-05 03:31:52 +0200

    Create Secant1.m

---
+++ Practica 4/Secant1.m
@@ -0,0 +1,33 @@
+function outputValue = Secant1(f, a, b, tolerancia, errorfun, maxiter)
+    % Ejercicio 3.
+   
+    if f(a) * f(b) < 0 
+        
+        i = 1;
+        h = f(a) * (b-a) / ( f(b) - f(a) );
+        c = (a + b) / 2;
+       
+        while ~(abs(f(c)) <= errorfun) && ~(h <= tolerancia) && i < maxiter && i < 4
+
+            if abs(f(a)) > abs(f(b)) % Si f(a) es mayor que f(B) intercambiamos a por b, y b por a.
+                bridgeVar = a; % Variable que usaremos de puente para intercambiar a y b dentro del bucle.
+                a = b;
+                b = bridgeVar;
+            end
+            
+            h = f(a) * (b-a) / ( f(b) - f(a) );
+            c = a - h;
+
+            i = i + 1;
+        end
+        
+        % Descomenta esto para ver los datos en detalle referente al resultado (ULTIMA ITERACION).
+        % fprintf('| i |   a  |   c  |   b  |   (b-a)/2  | \n', i, a, c, b, rango); 
+        % disp('------------------------------------------');
+        fprintf('| %1.0f | %.2f | %.2f | %.2f |    %.2f    | \n', i, a, c, b, h); 
+        outputValue = c;
+    else
+        disp('Intervalo no valido'); 
+    end
+end
+
