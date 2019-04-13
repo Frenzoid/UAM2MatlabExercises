acf4a5a3fe32c264c2e93fc7abb6535f17866100
Frenzoid <frenzoid@protonmail.com>
2019-04-13 19:42:27 +0200

    Update PuntoFijo1.m

--- Practica 4/PuntoFijo1.m
+++ Practica 4/PuntoFijo1.m
@@ -2,18 +2,20 @@ function outputValue = PuntoFijo1(g, c, tolerancia, maxiter)
     % Ejemplo.
     
         i = 1;
-        h = tolerancia + 1; % lo inicializamos con un valor para que entre en el bucle.
-       
-        while ~( g(c) == c) && (abs(h) > tolerancia) && (i <= maxiter)
+        h = abs(c - g(c));
+        
+        fprintf('| %1.0f | %.2f | %.2f |    %.2f    | \n', i, c, g(c), h); 
 
-        c = g(c); % c ahora es el resultado de g(c).
-        h = abs(c - g(c)); % calculamos la diferencia entre el valor y el resultado de la funcion.
-        
-        % Descomenta esto para ver los datos en detalle referente al resultado.
-        % fprintf('| %1.0f | %.2f | %.2f |    %.2f    | \n', i, c, g(c), h); 
-        
-        i = i + 1;
-        
+        while ~( g(c) == c) && (abs(h) > tolerancia) && (i <= maxiter)
+            i = i + 1;
+
+            c = g(c); % c ahora es el resultado de g(c).
+            h = abs(c - g(c)); % calculamos la diferencia entre el valor y el resultado de la funcion.
+
+            % Descomenta esto para ver los datos en detalle referente al resultado.
+            fprintf('| %1.0f | %.2f | %.2f |    %.2f    | \n', i, c, g(c), h); 
+
+
         end
         
         outputValue = c;
