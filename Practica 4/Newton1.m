f0109ba12350d6c9a229e85a62974c8bb5c30a1f
Frenzoid <frenzoid@protonmail.com>
2019-04-14 03:47:31 +0200

    Update Newton1.m

--- Practica 4/Newton1.m
+++ Practica 4/Newton1.m
@@ -1,37 +1,37 @@
 function outputValue = Newton1(f, a, tolerancia, errorfun, maxiter)
     % Ejercicio 4. ESTA FUNCION RECIBE UNA EXPRESION SIMBOLICA
-        syms x;
-        
-        if diff(f) ~= 0
-            i = 1;
-            fun = matlabFunction(f); % Transforma una expresion a una funcion handle.
-            
-            fdiff = diff(f);
-            fdiffun = matlabFunction(fdiff);  % Transforma una expresion a una funcion handle.
-            
+    syms x;
+
+    if diff(f) ~= 0
+        i = 1;
+        fun = matlabFunction(f); % Transforma una expresion a una funcion handle.
+
+        fdiff = diff(f);
+        fdiffun = matlabFunction(fdiff);  % Transforma una expresion a una funcion handle.
+
+        h = fun(a) / fdiffun(a);
+        c = a - h;
+
+        disp('------------------------------------');
+        fprintf('| i |   a  |   c  |     h      | \n', i, a, c, h); 
+        disp('------------------------------------');
+
+        while (abs(fun(c)) > errorfun) && (abs(h) > tolerancia) && (i < maxiter)
+
             h = fun(a) / fdiffun(a);
             c = a - h;
+            a = c;
 
-            disp('------------------------------------');
-            fprintf('| i |   a  |   c  |     h      | \n', i, a, c, h); 
-            disp('------------------------------------');
-            
-            while (abs(fun(c)) > errorfun) && (abs(h) > tolerancia) && (i < maxiter)
+            % Descomenta esto para ver los datos en detalle referente al resultado (ULTIMA ITERACION).
+            fprintf('| %1.0f | %.2f | %.2f |    %.2f    | \n', i, a, c, h); 
 
-                h = fun(a) / fdiffun(a);
-                c = a - h;
-                a = c;
-                
-                % Descomenta esto para ver los datos en detalle referente al resultado (ULTIMA ITERACION).
-                fprintf('| %1.0f | %.2f | %.2f |    %.2f    | \n', i, a, c, h); 
-                
-                i = i + 1;
+            i = i + 1;
 
-            end
-            
-            outputValue = c;
-        else
-            disp(diff(f));
-            disp('No se puede continuar, no se puede dividir entre cero.');
         end
+
+        outputValue = c;
+    else
+        disp(diff(f));
+        disp('No se puede continuar, no se puede dividir entre cero.');
+    end
 end
