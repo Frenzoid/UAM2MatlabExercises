2bb81cb3876990220d608fb8cd66bb002208f641
Frenzoid <saassabau100@gmail.com>
2019-04-15 00:39:26 +0200

    Update RegulaFalsi1.m

--- Practica 4/RegulaFalsi1.m
+++ Practica 4/RegulaFalsi1.m
@@ -2,14 +2,7 @@ function outputValue = RegulaFalsi1(f, a, b, tolerancia, errorfun, maxiter)
     % Ejercicio 3. IGUAL QUE SECANTE PERO ENVEZ DE B = C, SE APLICA BOLZANO.
            
         i = 1;
-        
-        if abs(f(a)) > abs(f(b)) % Si f(a) es mayor que f(b) intercambiamos a por b, y b por a.
-            bridgeVar = a; % Variable que usaremos de puente para intercambiar a y b dentro del bucle.
-            a = b;
-            b = bridgeVar;
-            disp('    |       A <=> B       |---------------------|     f(a) <=> f(b)     |');
-        end
-        
+
         h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
         c = a - h;
         
@@ -24,13 +17,6 @@ function outputValue = RegulaFalsi1(f, a, b, tolerancia, errorfun, maxiter)
         while ( abs( f(c) ) > errorfun ) && ( abs(h) > tolerancia ) && (i < maxiter)
             i = i + 1;
 
-            if abs(f(a)) > abs(f(b))
-                bridgeVar = a;
-                a = b;
-                b = bridgeVar;
-                disp('    |       A <=> B       |---------------------|     f(a) <=> f(b)     |');
-            end
-            
             h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
             c = a - h;
             
