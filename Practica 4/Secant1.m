e1bef89b88f0f0506bc0a28c0cc049c9dfae931d
Frenzoid <saassabau100@gmail.com>
2019-04-14 18:04:28 +0200

    Update Secant1.m

--- Practica 4/Secant1.m
+++ Practica 4/Secant1.m
@@ -7,7 +7,7 @@ function outputValue = Secant1(f, a, b, tolerancia, errorfun, maxiter)
             bridgeVar = a; % Variable que usaremos de puente para intercambiar a y b dentro del bucle.
             a = b;
             b = bridgeVar;
-                disp('    |       A <=> B       |                     |     f(a) <=> f(b)     |');
+            disp('    |       A <=> B       |---------------------|     f(a) <=> f(b)     |');
         end
         
         h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
@@ -24,7 +24,7 @@ function outputValue = Secant1(f, a, b, tolerancia, errorfun, maxiter)
                 bridgeVar = a;
                 a = b;
                 b = bridgeVar;
-                disp('    |       A <=> B       |                     |     f(a) <=> f(b)     |');
+                disp('    |       A <=> B       |---------------------|     f(a) <=> f(b)     |');
             end
             
             h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
