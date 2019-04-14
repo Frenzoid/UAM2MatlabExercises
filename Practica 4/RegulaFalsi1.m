79773f83b43a7b99b2991786bf28e1541ae11098
Frenzoid <frenzoid@protonmail.com>
2019-04-14 03:32:54 +0200

    Update RegulaFalsi1.m

--- Practica 4/RegulaFalsi1.m
+++ Practica 4/RegulaFalsi1.m
@@ -1,16 +1,32 @@
-function outputValue = RegulaFalsi1(f, a, b, tolerancia, errorfun, maxiter)
+function outputValue = Secant1(f, a, b, tolerancia, errorfun, maxiter)
     % Ejercicio 3.
-    
-    if f(a) * f(b) < 0 % Comprueba si la funcion converge dentro del intervalo.
-
+           
         i = 1;
+        
+        if abs(f(a)) > abs(f(b)) % Si f(a) es mayor que f(b) intercambiamos a por b, y b por a.
+            bridgeVar = a; % Variable que usaremos de puente para intercambiar a y b dentro del bucle.
+            a = b;
+            b = bridgeVar;
+        end
+        
         h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
-        c = (a + b) / 2;
+        c = a - h;
        
-        while ( abs( f(c) ) > errorfun ) && ( abs(h) > tolerancia ) && i <= maxiter
+        if f(a) * f(c) < 0
+                b = c; 
+        else
+            a = c;
+        end
 
-            if abs(f(a)) > abs(f(b)) % Si f(a) es mayor que f(b) intercambiamos a por b, y b por a.
-                bridgeVar = a; % Variable que usaremos de puente para intercambiar a y b dentro del bucle.
+        fprintf('| %1.0f | %f | %f | %f | %f | %f | %f | %f | \n', i, a, b, c, h, f(a), f(b), f(c));
+       
+
+        
+        while ( abs( f(c) ) > errorfun ) && ( abs(h) > tolerancia ) && (i < maxiter)
+            i = i + 1;
+
+            if abs(f(a)) > abs(f(b))
+                bridgeVar = a;
                 a = b;
                 b = bridgeVar;
             end
@@ -24,15 +40,9 @@ function outputValue = RegulaFalsi1(f, a, b, tolerancia, errorfun, maxiter)
                 a = c;
             end
 
-            % Descomenta esto para ver los datos en detalle referente al resultado (ULTIMA ITERACION).
-            % fprintf('| %1.0f | %.2f | %.2f | %.2f |    %.2f    | \n', i, a, c, b, h); 
+            fprintf('| %1.0f | %f | %f | %f | %f | %f | %f | %f | \n', i, a, b, c, h, f(a), f(b), f(c));
             
-            i = i + 1;
         end
         
         outputValue = c;
-        
-    else
-        disp('Intervalo no valido'); % No hay convergencia en el intervalo introducido.
-    end
 end
