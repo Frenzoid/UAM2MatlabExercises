114500d334fb90f9330cf4d778f07cbf668b0196
Frenzoid <frenzoid@protonmail.com>
2019-04-14 03:47:13 +0200

    Update Steﬀensen.m

--- Practica 4/Steﬀensen.m
+++ Practica 4/Steﬀensen.m
@@ -1,24 +1,24 @@
 function outputValue = Steffesen(f, a, tolerancia, errorfun, maxiter)
     % Ejercicio 6.
     
-            i = 1;
-            c = a;
-            hfun = @(x)(f(x))^2 / f(x + f(x)) - f(x);
-            h = hfun(c);
+    i = 1;
+    c = a;
+    hfun = @(x)(f(x))^2 / f(x + f(x)) - f(x);
+    h = hfun(c);
 
-            disp('----------------------------');
-            fprintf('| i |  c  |     h      | \n', i, c, h); 
-            disp('----------------------------');
-            
-            while (abs(f(c)) > errorfun) && (h > tolerancia) && (i < maxiter)
-               
-                c = c - h(c);
-                h = hfun(c);
-                
-                fprintf('| %1.0f | %.2f |    %.2f    | \n', i, c, h); 
+    disp('----------------------------');
+    fprintf('| i |  c  |     h      | \n', i, c, h); 
+    disp('----------------------------');
 
-                
-            end
-            
-            outputValue = c;
+    while (abs(f(c)) > errorfun) && (h > tolerancia) && (i < maxiter)
+
+        c = c - h(c);
+        h = hfun(c);
+
+        fprintf('| %1.0f | %.2f | %.2f |    %.2f    | \n', i, a, c, h); 
+
+
+    end
+
+    outputValue = c;
 end
