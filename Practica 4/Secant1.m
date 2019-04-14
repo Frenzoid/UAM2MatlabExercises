0c2f5e97d47025b6f30e83bb60052c7a48a77e3e
Frenzoid <frenzoid@protonmail.com>
2019-04-14 03:22:31 +0200

    Update Secant1.m

--- Practica 4/Secant1.m
+++ Practica 4/Secant1.m
@@ -2,26 +2,32 @@ function outputValue = Secant1(f, a, b, tolerancia, errorfun, maxiter)
     % Ejercicio 2.
            
         i = 1;
+        
         h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
         c = (a + b) / 2;
+        
+        fprintf('| %1.0f | %.2f | %.2f | %.2f |    %.2f    | \n', i, a, c, b, h); 
        
-        while ( abs( f(c) ) > errorfun ) && ( abs(h) > tolerancia ) && (i <= maxiter)
+        if abs(f(a)) > abs(f(b)) % Si f(a) es mayor que f(b) intercambiamos a por b, y b por a.
+            bridgeVar = a; % Variable que usaremos de puente para intercambiar a y b dentro del bucle.
+            a = b;
+            b = bridgeVar;
+        end
+        
+        while ( abs( f(c) ) > errorfun ) && ( abs(h) > tolerancia ) && (i < maxiter)
+            i = i + 1;
 
-            if abs(f(a)) > abs(f(b)) % Si f(a) es mayor que f(b) intercambiamos a por b, y b por a.
-                bridgeVar = a; % Variable que usaremos de puente para intercambiar a y b dentro del bucle.
+            h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
+            c = (a + b) / 2;
+
+            fprintf('| %1.0f | %.2f | %.2f | %.2f |    %.2f    | \n', i, a, c, b, h); 
+
+            if abs(f(a)) > abs(f(b))
+                bridgeVar = a;
                 a = b;
                 b = bridgeVar;
             end
             
-            h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
-            c = a - h;
-            b = c;
-
-            
-            % Descomenta esto para ver los datos en detalle referente al resultado (ULTIMA ITERACION).
-            % fprintf('| %1.0f | %.2f | %.2f | %.2f |    %.2f    | \n', i, a, c, b, h); 
-            
-            i = i + 1;
         end
         
         outputValue = c;
