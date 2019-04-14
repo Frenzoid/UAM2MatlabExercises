6976c3e199d8a58d77041c7dde12d31da91fd470
Frenzoid <saassabau100@gmail.com>
2019-04-14 18:02:20 +0200

    Update RegulaFalsi1.m

--- Practica 4/RegulaFalsi1.m
+++ Practica 4/RegulaFalsi1.m
@@ -1,6 +1,5 @@
 function outputValue = RegulaFalsi1(f, a, b, tolerancia, errorfun, maxiter)
-    % Ejercicio 3.
-         % La unica diferencia entre secante y regula falsi es que en lugar de poner b = c; pones la regla de Bolzano.
+    % Ejercicio 2.
            
         i = 1;
         
@@ -8,20 +7,19 @@ function outputValue = RegulaFalsi1(f, a, b, tolerancia, errorfun, maxiter)
             bridgeVar = a; % Variable que usaremos de puente para intercambiar a y b dentro del bucle.
             a = b;
             b = bridgeVar;
+            disp('    |       A <=> B       |                     |     f(a) <=> f(b)     |');
         end
         
         h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
         c = a - h;
-       
-        if f(a) * f(c) < 0
-                b = c; 
+        
+        fprintf('| %1.0f | %f | %f | %f | %f | %f | %f | %f | \n', i, a, b, c, h, f(a), f(b), f(c));
+
+        if (f(a) * f(c)) < 0 
+            b = c;
         else
             a = c;
         end
-
-        fprintf('| %1.0f | %f | %f | %f | %f | %f | %f | %f | \n', i, a, b, c, h, f(a), f(b), f(c));
-       
-
         
         while ( abs( f(c) ) > errorfun ) && ( abs(h) > tolerancia ) && (i < maxiter)
             i = i + 1;
@@ -30,18 +28,19 @@ function outputValue = RegulaFalsi1(f, a, b, tolerancia, errorfun, maxiter)
                 bridgeVar = a;
                 a = b;
                 b = bridgeVar;
+                disp('    |       A <=> B       |                     |     f(a) <=> f(b)     |');
             end
             
             h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
             c = a - h;
             
-            if f(a) * f(c) < 0
-                b = c; 
+            fprintf('| %1.0f | %f | %f | %f | %f | %f | %f | %f | \n', i, a, b, c, h, f(a), f(b), f(c));
+
+            if (f(a) * f(c)) < 0 
+                b = c;
             else
                 a = c;
             end
-
-            fprintf('| %1.0f | %f | %f | %f | %f | %f | %f | %f | \n', i, a, b, c, h, f(a), f(b), f(c));
             
         end
         
