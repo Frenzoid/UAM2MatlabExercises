22d5e3d7b812bb2ca3089ec1823a806717e837fd
Frenzoid <saassabau100@gmail.com>
2019-05-06 21:04:37 +0200

    quick edit

--- Practica 4/RegulaFalsi1.m
+++ Practica 4/RegulaFalsi1.m
@@ -1,10 +1,31 @@
 function outputValue = RegulaFalsi1(f, a, b, tolerancia, errorfun, maxiter)
     % Ejercicio 3. IGUAL QUE SECANTE PERO ENVEZ DE B = C, SE APLICA BOLZANO.
            
-        i = 1;
-        
-         if abs(f(a)) > abs(f(b)) % Si f(a) es mayor que f(b) intercambiamos a por b, y b por a.
-            bridgeVar = a; % Variable que usaremos de puente para intercambiar a y b dentro del bucle.
+    i = 1;
+
+     if abs(f(a)) > abs(f(b)) % Si f(a) es mayor que f(b) intercambiamos a por b, y b por a.
+        bridgeVar = a; % Variable que usaremos de puente para intercambiar a y b dentro del bucle.
+        a = b;
+        b = bridgeVar;
+        disp('    |       A <=> B       |---------------------|     f(a) <=> f(b)     |');
+    end
+
+    h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
+    c = a - h;
+
+    fprintf('| %1.0f | %f | %f | %f | %f | %f | %f | %f | \n', i, a, b, c, h, f(a), f(b), f(c));
+
+    if (f(a) * f(c)) < 0 
+        b = c;
+    else
+        a = c;
+    end
+
+    while ( abs( f(c) ) > errorfun ) && ( abs(h) > tolerancia ) && (i < maxiter)
+        i = i + 1;
+
+        if abs(f(a)) > abs(f(b))
+            bridgeVar = a;
             a = b;
             b = bridgeVar;
             disp('    |       A <=> B       |---------------------|     f(a) <=> f(b)     |');
@@ -12,7 +33,7 @@ function outputValue = RegulaFalsi1(f, a, b, tolerancia, errorfun, maxiter)
 
         h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
         c = a - h;
-        
+
         fprintf('| %1.0f | %f | %f | %f | %f | %f | %f | %f | \n', i, a, b, c, h, f(a), f(b), f(c));
 
         if (f(a) * f(c)) < 0 
@@ -20,29 +41,8 @@ function outputValue = RegulaFalsi1(f, a, b, tolerancia, errorfun, maxiter)
         else
             a = c;
         end
-        
-        while ( abs( f(c) ) > errorfun ) && ( abs(h) > tolerancia ) && (i < maxiter)
-            i = i + 1;
-            
-            if abs(f(a)) > abs(f(b))
-                bridgeVar = a;
-                a = b;
-                b = bridgeVar;
-                disp('    |       A <=> B       |---------------------|     f(a) <=> f(b)     |');
-            end
 
-            h = ( f(a) * (b-a) ) / ( f(b) - f(a) );
-            c = a - h;
-            
-            fprintf('| %1.0f | %f | %f | %f | %f | %f | %f | %f | \n', i, a, b, c, h, f(a), f(b), f(c));
+    end
 
-            if (f(a) * f(c)) < 0 
-                b = c;
-            else
-                a = c;
-            end
-            
-        end
-        
-        outputValue = c;
+    outputValue = c;
 end
